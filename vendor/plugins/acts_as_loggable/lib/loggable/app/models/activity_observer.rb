63da1e1870bb1f7368a73aa67392fd031749857d
Chris Johnston <chrisj@celect.org>
2011-03-11 17:03:00 -0500

    added observer to plugin

---
+++ vendor/plugins/acts_as_loggable/lib/loggable/app/models/activity_observer.rb
@@ -0,0 +1,16 @@
+class ActivityObserver < ActiveRecord::Observer
+  observe :article, :comment
+
+  def after_create(model)
+    save_action(model, "created")
+  end
+
+  def after_update(model)
+    save_action(model, "updated")
+  end
+
+  private
+  def save_action(model, action)
+    Activity.create!(:action => action, :logable => model)
+  end
+end
