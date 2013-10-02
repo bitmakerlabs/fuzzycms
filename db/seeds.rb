12799b088e9f31b1bd23c7c1ef7a9d2ce7d8e3cb
Chris Johnston <chris@fuzzylizard.com>
2013-10-01 21:22:44 -0400

    removing database.yml file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+100.times {|x|
+  Article.create title: "Article #{x}", body: "blog article body for article #{x}"
+}
