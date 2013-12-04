0aad865849d67ab6c166882a5bc6e0aeaeec2cb4
Chris Johnston <chris@fuzzylizard.com>
2013-12-04 13:01:29 -0500

    adds images

--- app/models/article.rb
+++ app/models/article.rb
@@ -16,7 +16,8 @@ class Article < ActiveRecord::Base
   end
 
   def self.tag_counts
-  	Tag.select("tags.id, tags.name, count(taggings.tag_id) as count").joins(:taggings).group("taggings.tag_id, tags.id, tags.name")
+  	Tag.select("tags.id, tags.name, count(taggings.tag_id) as count").
+    joins(:taggings).group("taggings.tag_id, tags.id, tags.name")
   end
 
   def tag_list
