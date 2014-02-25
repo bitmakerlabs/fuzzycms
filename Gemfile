ccc56b2783eb3e09fc5419dd80c0f9f02da4b8e2
Chris Johnston <chris@fuzzylizard.com>
2014-02-25 11:00:18 -0500

    added email gem thingy

--- Gemfile
+++ Gemfile
@@ -25,6 +25,7 @@ end
 group :development do
 	gem 'better_errors'
 	gem "binding_of_caller"
+  gem "letter_opener"
 end
 
 group :doc do
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -29,16 +29,18 @@ Fuzzycms::Application.configure do
 
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.perform_deliveries = true
-
+  
   config.action_mailer.default_url_options = { host: 'localhost:3000' }
-  config.action_mailer.delivery_method = :smtp
-  config.action_mailer.smtp_settings = {
-    address:              'smtp.gmail.com',
-    port:                 587,
-    domain:               'gmail.com',
-    user_name:            'blcohort3@gmail.com',
-    password:             'cohort3rocks',
-    authentication:       'plain',
-    enable_starttls_auto: true
-  }
+  config.action_mailer.delivery_method = :letter_opener
+
+  # config.action_mailer.delivery_method = :smtp
+  # config.action_mailer.smtp_settings = {
+  #   address:              'smtp.gmail.com',
+  #   port:                 587,
+  #   domain:               'gmail.com',
+  #   user_name:            'blcohort3@gmail.com',
+  #   password:             'cohort3rocks',
+  #   authentication:       'plain',
+  #   enable_starttls_auto: true
+  # }
 end
